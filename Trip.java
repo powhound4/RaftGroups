cc4fa38a46604da82e851a81bd0ac0baba6b01d2
powhound4 <george.hatch@q.com>
2019-01-04 14:30:04 -0700

    Update Trip.java

--- Trip.java
+++ Trip.java
@@ -7,24 +7,17 @@ import java.util.Scanner;
 // Assignment: WanderlustBoat
 // Author: George Hatch
 // Date:   Aug 25, 2018
-// Email:  Powhound@rams.colostate.edu
+// Email:  georgehatch91@gmail.com
 
 public class Trip implements Iterator<Party> {
 	Iterator<String> iterS;
 	private String line;
 	private int totPeople;
-	//private String nextToken;
-	//private int stLeng;
-	//private int iter;
 	private int numBoats;
 	private double waterLevel;
-	//private int groupsLeft;
 	private ArrayList<Party> customers = new ArrayList<Party>();  
-	private static ArrayList<Boat> Boats = new ArrayList<Boat>();
-	//private ArrayList<ArrayList<Party>> parties = new ArrayList<ArrayList<Party>>();
+	private static ArrayList<Boat> Boats = new ArrayList<Boat>();	
 	
-	
-
 	private Trip(String line){
 		BuildTrip(line);
 	}
@@ -32,7 +25,6 @@ public class Trip implements Iterator<Party> {
 	private void BuildTrip(String line){
 		ReadFile(line);
 		System.out.println(customers.toString());
-		//groupsLeft = customers.size();
 		BuildBoats(customers);
 		System.out.println(Boats.toString());
 		System.out.println("Total # of People = " + Boats.get(0).getTotPeople());
@@ -40,11 +32,9 @@ public class Trip implements Iterator<Party> {
 	
 	private void ReadFile(String line){
 		iterS = new Scanner(line);
-		//iter = 0;
 		if(line.contains("TripSheet.txt")){
 		Scanner reader;
 		try{
-			
 			reader = new Scanner(new File(line));
 			while(reader.hasNextLine()){
 				Party i = new Party(reader.next(), reader.nextInt(), null);
@@ -65,32 +55,25 @@ public class Trip implements Iterator<Party> {
 		}
 		else{
 			this.line = line.trim();
-			//stLeng = this.line.length();
 		}
 		
 	}
+	//loops through all the groups and assigns them accordingly
 	private void BuildBoats(ArrayList<Party> cus){
 		for(int i = 0; i < cus.size();i++){
 			 if(cus.get(i).getSize()<5 && cus.get(i).inaBoat() == false){
-				//System.out.println("Checking groups for "+ cus.get(i));
 				checkGroups(cus.get(i));
-			 
-			 
 				for(int j =1; j<=cus.size()-1;j++){
 					if(cus.get(i).getSize()+cus.get(j).getSize() <=7 && (cus.get(j).inaBoat()==false && cus.get(i).inaBoat()==false) && 
 							!(cus.get(i).getName().equals(cus.get(j).getName()))){
 						ArrayList<Party> group = new ArrayList<Party>();
 						group.add(cus.get(i));
 						group.add(cus.get(j));
-						//System.out.println("Group Name = " + cus.get(i));
-						//System.out.println("Group Name = " + cus.get(j));
 						cus.get(i).assignBoat();
 						cus.get(j).assignBoat();
 						
 						int grpSize = cus.get(i).getSize()+cus.get(j).getSize();
 						while (grpSize <= 4){
-							//System.out.println("ENTERING");
-
 							int index = refineGroup(cus, j, grpSize);
 							if(index == -1){
 								break;
@@ -127,12 +110,11 @@ public class Trip implements Iterator<Party> {
 				index = i;
 			}
 		}
-		//System.out.println(grp+ " will be joining " + Boats.get(index));
 		Boats.get(index);
 		editBoat(Boats.get(index), grp, index);
-		
-		
 	}
+	
+	//adds a party to an existing boat
 	private void editBoat(Boat old, Party add, int iob){
 		ArrayList<ArrayList<Party>> parties = old.getParties();
 		ArrayList<Party> party = parties.get(0);
@@ -140,29 +122,17 @@ public class Trip implements Iterator<Party> {
 		int numPeeps = old.getNumPeople();
 		
 		for (int i = 0; i < party.size(); i++){
-			//System.out.println("The "+ party.get(i) + " is in a boat?" + party.get(i).inaBoat());
 			newGroup.add(party.get(i));
 			party.get(i).assignBoat();
 		}
 		newGroup.add(add);
-		//System.out.println("The add on group "+ add + " is in a boat?" + add.inaBoat());
-
 		add.assignBoat();
-		//System.out.println("Now "+ add + " is in a boat?" + add.inaBoat());
-
-
-		//System.out.println("New Group = " + newGroup);
 		Boat tempBoat = new Boat(newGroup);
-		
-		//System.out.println("NUM Peeps after = "+ tempBoat.getNumPeople());
-
 		Boats.add(tempBoat);
 		tempBoat.setNumPeople(numPeeps + add.getSize());
-		//int oldI = Boats.indexOf(old);
-		//tempBoat.setBoatNum(Boats.get(oldI -1).getBoatNum() + 1);
 		Boats.remove(iob);
 	}
-
+	//recursive function that splits the group up until the boat size is <= 8
 	private void splitGroup(Party temp, int size, int num, int pBoat) {
 		int groupSize = size/num;
 		int denom = num;
@@ -191,7 +161,6 @@ public class Trip implements Iterator<Party> {
 				tempBoat.setNumPeople(groupSize);
 				Boats.add(tempBoat);
 				previousBoat = Boats.size()-1;
-				//System.out.println("Boat number: " + previousBoat);
 				num--;
 			}		
 		}
@@ -201,14 +170,12 @@ public class Trip implements Iterator<Party> {
 		
 		
 	}
-	
+	//finds the smallest boat to add a party to, could call edit boat instead.
 	private int refineGroup(ArrayList<Party> cus, int j, int boatSize){
 		int index = -1;
-		//System.out.println("boatsize"+ boatSize);
 		for(int i = j; i <= cus.size()-1; i++){
 			if(boatSize + cus.get(i).getSize() < 8 && cus.get(i).inaBoat() == false){
 				index = i;
-				//System.out.println("index"+ index);
 				break;
 			}
 		}
@@ -241,9 +208,7 @@ public class Trip implements Iterator<Party> {
 	}
 	public static void main(String[] args){
 		Trip Test = new Trip("TripSheet.txt");
-		Test.toString();
-		//System.out.println("Boats = " + Boats.toString());
-		
+		Test.toString();		
 	}
 	
 
